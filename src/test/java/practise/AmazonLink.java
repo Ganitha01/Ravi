75894fef5df6b8f74f74730af1b84d35fb8aa789
Ravichandrababuu <117425873+Ravichandrababuu@users.noreply.github.com>
2023-05-31 17:29:08 +0530

    Update AmazonLink.java

--- src/test/java/practise/AmazonLink.java
+++ src/test/java/practise/AmazonLink.java
@@ -20,6 +20,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class AmazonLink {
 //ravichandrababu
 	//ravichandrababu
+	//test resources hello
 	public static void main(String[] args) throws EncryptedDocumentException, IOException, InterruptedException {
      WebDriverManager.chromedriver().setup();
      WebDriver driver=new ChromeDriver();
